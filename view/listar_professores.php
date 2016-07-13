0773e5c8803f0d5044fe03993fc1e4eff3e2e8dd
sandrosan <sandrosan75@hotmail.com>
2016-07-13 17:05:27 -0300

    update arquivo de listagem

--- view/listar_professores.php
+++ view/listar_professores.php
@@ -7,9 +7,35 @@ $professorControl = new ProfessorControl();
 
 
  
- foreach ($professorControl->listarTodos() as $professor) {
-     echo $professor->id . '/';
-     echo $professor->nome . '/';
- }
+// foreach ($professorControl->listarTodos() as $professor) {
+    // echo $professor->id . '/';
+    // echo $professor->nome . '/';
+// }
+
+
+?>
+<table>
+    <tr>
+    <th>id </th> 
+    <th>nome</th>
+    
+    </tr>
+    
+
+
+<?php
+foreach ($professorControl->listarTodos() as $professor) {
+    ?>
+}
+    <tr>
+    
+    <td><?php echo $professor->id;?></td
+    <td><?php echo $professor->nome;?></td>
+</tr>
+
+<?php } ?>
+
+</table>
+
  
  ?>
