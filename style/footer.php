5566e8ab58de329dba48c875e6d4c95a0f41f7b4
Gustavo <gustavo.softwares@hotmail.com>
2016-07-16 12:39:36 -0300

    menu bar e estilização tela de listagem

--- style/footer.php
+++ style/footer.php
@@ -1,4 +1,4 @@
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+ <script src="../style/js/bootstrap.min.js"></script>
   </body>
 </html>
--- view/listar_especialidades.php
+++ view/listar_especialidades.php
@@ -1,28 +1,47 @@
 <?php
+include_once '../style/template.php';
 include_once '../control/EspecialidadeControl.php';
 $especialidadeControl = new EspecialidadeControl();
 
 //print_r($especialidadeControl->listarTodos());
 ?>
-<table>
+<div class="col-sm-10 col-sm-offset-1">
+       <h1>Especialidades</h1>
+       <a href="" class ="pull-right btn btn-primary btn-xs">cadastrar especialidade</a>
+       
+
+<table class ="table table-striped">
     <tr>
     <th>id </th> 
     <th>nome</th>
+    <th></th>
+    <th></th>
     
     </tr>
     
-
-
 <?php
 foreach ($especialidadeControl->listarTodos() as $especialidade) {
     ?>
 
     <tr>
     
-    <td><?php echo $especialidade->id;?></td
-    <td><?php echo $especialidade->nome;?></td>
-</tr>
+        <td><?php echo $especialidade->id;?></td>
+        <td><?php echo $especialidade->nome;?></td>
+        <td>
+            <div class="pull-right">
+                
+                <a href= "">editar</a>
+                |
+                <a href="">deletar<a/>
+                   
+                
+            </div>
+        <td/>
+    </tr>
 
 <?php } ?>
 
 </table>
+</div>
+
+<?php include_once '../style/footer.php';?>
