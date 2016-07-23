5874e6ef92e27efb79af8ef656c1b6c75d4192fc
Gustavo <gustavo.softwares@hotmail.com>
2016-07-23 11:56:34 -0300

    atualizacao

--- view/listar_professores.php
+++ view/listar_professores.php
@@ -10,11 +10,16 @@ $professorControl = new ProfessorControl();
 // foreach ($professorControl->listarTodos() as $professor) {
     // echo $professor->id . '/';
     // echo $professor->nome . '/';
+    
 // }
 
 
 ?>
-<table>
+<div class="col-sm-10 col-sm-offset-1">
+       <h1>Professores</h1>
+       <a href="" class ="pull-right btn btn-primary btn-xs">cadastrar professores</a>
+       
+<table class ="table table-striped">
     <tr>
     <th>id </th> 
     <th>nome</th>
